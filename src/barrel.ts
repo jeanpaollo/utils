98c93bfcceff0e4cf128bccb9deb0bcb3e0da7f0
Jean Paollo Olimpio Gomes Ferreira <jeanpaollo@gmail.com>
2024-07-03 18:04:11 -0300

    Creation of Utils code

---
+++ src/barrel.ts
@@ -0,0 +1,6 @@
+export * from "@jeanpaollo/boolean-utils";
+export * from "@jeanpaollo/proxy-utils";
+export * from "logical-utils";
+export * from "nullish-utils";
+
+export { and, or, xnor } from "logical-utils";
